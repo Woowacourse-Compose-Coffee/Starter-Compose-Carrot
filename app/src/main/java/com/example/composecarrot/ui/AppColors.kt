e4bd5c399dc209cdf642b221528a3b5edc714854
todays_sun_day <178suna@gmail.com>
2026-03-30 01:20:59 +0900

    refactor: Color 와 TextStyle 하드코딩 부분 수정

---
+++ app/src/main/java/com/example/composecarrot/ui/AppColors.kt
@@ -0,0 +1,7 @@
+package com.example.composecarrot.ui
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val Gray = Color(0xFF868C93)
+}
---
+++ app/src/main/java/com/example/composecarrot/ui/AppTypography.kt
@@ -0,0 +1,12 @@
+package com.example.composecarrot.ui
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+object AppTypography {
+    val Caption14Medium = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+    )
+}
